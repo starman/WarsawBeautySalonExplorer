a3c4f8368a5544acd75285a537a63a49e90cc44f
Jakub Ciemała <ciemalajakub@gmail.com>
2026-05-21 14:10:29 +0200

    Add Swagger

--- WarsawBeautySalonExplorer/Program.cs
+++ WarsawBeautySalonExplorer/Program.cs
@@ -12,6 +12,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI(opt => opt.SwaggerEndpoint("/openapi/v1.json", "My API V1"));
 }
 
 app.UseHttpsRedirection();
