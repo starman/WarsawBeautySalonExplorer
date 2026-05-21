7ccc74dfc2748296405b601d2d500b35f62cd14a
Jakub Ciemała <ciemalajakub@gmail.com>
2026-05-21 14:23:29 +0200

    Add salon data model

---
+++ WarsawBeautySalonExplorer/Models/Salon.cs
@@ -0,0 +1,12 @@
+﻿namespace WarsawBeautySalonExplorer.Models;
+
+public class Salon
+{
+    public string Id { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public string Adress { get; set; } = string.Empty;
+    public string District { get; set; } = string.Empty;
+    public List<string> Services { get; set; } = new();
+    public double Rating { get; set; }
+    public int ReviewsCount { get; set; }
+}
